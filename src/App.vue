e9e4d73d7c07679e31a76705b31c9e9e2f19a1ee
markobezic <marko.bezic@gmail.com>
2021-02-08 13:59:24 -0500

    set up project, imported bootstrap and bootstrap-vue, started with boiler plate code for sidebar menu

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Header />
+    <Sidebar />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Sidebar from './components/Sidebar.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Sidebar,
+    Header,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="shipping-banner">
+    <p class="text-white m-auto p-1">USPS is seeing delays; orders may be delayed</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+  .shipping-banner {
+    transform: translateY(-60px);
+    background-color: #89cf31;
+    font-size: 11px;
+  }
+
+</style>
