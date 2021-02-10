617444a3345b610260ce3e846f9069741684e118
markobezic <marko.bezic@gmail.com>
2021-02-10 17:20:46 -0500

    re-did the styling for the banner for cleaner sizing at breakpoints

--- src/components/Banner/Banner.vue
+++ src/components/Banner/Banner.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="banner">
-    <img class="background" src="../../images/home-hero-products@2x.jpg" alt="home hero products"/>
+    <img class="background-img" src="../../images/home-hero-products@2x.jpg" alt="home hero products"/>
     <img class="discover" src="../../images/discover-d3b91d6a26f47dc0e4a783485e952f4535d84f534dd129f588a5f6d1dd5f23de.png" alt="Discover">
     <p class="tagline text-center">the delightfully simple way to a greener, healthier life.</p>
     <green-button class="join-now-btn" txt="JOIN NOW"></green-button>
@@ -18,118 +18,107 @@ export default {
 </script>
 
 <style scoped>
-  .background {
+  .banner {
+    height:40vh;
+    min-height: 250px;
+    max-height: 300px;
+    overflow: hidden;
+    position: relative;
+  }
+
+  .background-img {
     z-index: -1;
-    max-width: 226%;
+    height: 40vh;
+    min-height: 250px;
+    max-height: 300px;
     position: absolute;
     left: 50%;
-    transform: translateX(-51%);
+    transform: translate(-50%);
   }
   .discover {
-    height: 14%;
-    position: absolute;
+    height: 40%;
+    position: absolute; 
     left: 50%;
-    top: 18%;
-    transform: translate(-51%);
+    top: 10%;
+    transform: translateX(-50%);
   }
   .tagline {
-    font-size: 17px;
-    line-height: 18px;
-    color: #666;
-    width: 230px;
+    color: #555;
+    width: 240px;
     position: absolute;
+    top: 55%;
     left: 50%;
-    transform: translate(-48%, 300%);
+    transform: translate(-50%, -50%);
   }
   .join-now-btn {
     position: absolute;
+    top: 82%;
     left: 50%;
-    transform: translate(-50%, 360%);
-  }
-  
-
-  @media(min-width: 500px) {
-    .background {
-    max-height: 45%;
-    position: absolute;
-    left: 50%;
-    transform: translateX(-51%);
-    }
-    .discover {
-    height: 23%;
-    position: absolute;
-    left: 50%;
-    top: 16%;
-    transform: translate(-51%);
-    }
-     .tagline {
-    font-size: 17px;
-    line-height: 18px;
-    color: #666;
-    width: 230px;
-    position: absolute;
-    left: 50%;
-    top: 35%;
-    transform: translate(-48%);
-    }
-     .join-now-btn {
-    position: absolute;
-    left: 50%;
-    top: 45%;
-    transform: translate(-50%);
-    }
-
-  }
-  
-  @media(min-width: 767px) {
-    .background {
-    max-height: 68%;
-    position: absolute;
-    left: 50%;
-    transform: translateX(-50%);
-    }
-    .discover {
-    height: 27%;
-    position: absolute;
-    left: 50%;
-    top: 18%;
-    transform: translate(-51%);
-    }
-     .tagline {
-    font-size: 30px;
-    line-height: 30px;
-    color: #666;
-    width: 420px;
-    position: absolute;
-    left: 50%;
-    top: 42%;
-    transform: translate(-48%);
-    }
-     .join-now-btn {
-    position: absolute;
-    left: 50%;
-    top: 60%;
-    transform: translate(-50%);
-    }
+    transform: translate(-50%, -50%);
   }
 
-  @media (min-width: 991px) {
-    .background {
-    max-height: 75%;
-    position: absolute;
-    left: 50%;
-    transform: translateX(-50%);
+  @media (min-width: 500px) {
+    .banner {
+      min-height: 350px;
+      max-height: 380px;
+    }
+    .background-img {
+      min-height: 350px;
+      max-height: 350px;
     }
     .discover {
-    height: 32%;
-    left: 50%;
-    top: 20%;
+      height: 50%;
     }
     .tagline {
-      top: 50%;
+      top: 58%;
+    }
+  }
+
+  @media (min-width: 767px) {
+    .banner {
+      min-height: 440px;
+      max-height: 440px;
+    }
+    .background-img {
+      min-height: 460px;
+      max-height: 460px;
+    }
+    .tagline {
+      width: 390px ;
+      font-size: 26px;
+      line-height: 30px;
+    }
+  }
+
+  @media (min-width: 992px) {
+    .banner {
+      min-height: 500px;
+      max-height: 500px;
+    }
+    .background-img {
+      min-height: 560px;
+      max-height: 570px;
+    }
+  }
+  @media (min-width: 1200px) {
+   .banner {
+      min-height: 520px;
+      max-height: 520px;
+    }
+    .background-img {
+      min-height: 580px;
+      max-height: 590px;
+    }
+    .discover {
+      max-height: 220px;
+      top: 15%;
+    }
+    .tagline {
+      width: 430px;
+      font-size: 30px;
     }
     .join-now-btn {
-      top: 68%;
+      top: 78%;
     }
   }
 </style>
