305cc61de52e5bf9a3129b6dc2ddf267e7b81c1d
markobezic <marko.bezic@gmail.com>
2021-02-11 09:14:44 -0500

    added main nav headings to sidebar

--- src/components/Header/Sidebar.vue
+++ src/components/Header/Sidebar.vue
@@ -1,14 +1,24 @@
   <template>
   <div>
     <button class="nav-button border-0 mt-1" v-b-toggle.sidebar-1><toggler-icon/></button>
-    <b-sidebar id="sidebar-1" title="NavBar" shadow>
-      <div class="px-3 py-2">
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-          in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-        </p>
-        <b-img src="/src/images/entrust_site_seal.png" fluid thumbnail></b-img>
-      </div>
+    <b-sidebar id="sidebar-1" shadow>
+      <div class="menu-item px-3 py-2">ALREADY A MEMBER? LOG-IN </div>
+      <div class="menu-item px-3 py-2">SUBCRIBE<font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></div>
+      <div class="menu-item px-3 py-2">ESSENTIALS</div>
+      <div class="menu-item px-3 py-2">NEW </div>
+      <div class="menu-item px-3 py-2">GIFTS <font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></div>
+      <div class="menu-item px-3 py-2">KITCHEN<font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></div>
+      <div class="menu-item px-3 py-2">ON THE GO <font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></div>
+      <div class="menu-item px-3 py-2">CLEANING <font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></div>
+      <div class="menu-item px-3 py-2">BODY <font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></div>
+      <div class="menu-item px-3 py-2">WELLNESS <font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></div>
+      <div class="menu-item px-3 py-2">KIDS <font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></div>
+      <div class="menu-item px-3 py-2">PETS <font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></div>
+      <div class="menu-item px-3 py-2">SALE <font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></div>
+      <div class="menu-item-small px-3 pt-4"><font-awesome-icon class="truck mr-1 mt-1" icon="truck"></font-awesome-icon>Free Shipping over $50</div>
+      <div class="menu-item px-3 py-2">MIGHTLIFE BLOG</div>
+      <div class="menu-item px-3 py-2">LOG-IN</div>
+      
     </b-sidebar>
   </div>
 </template>
@@ -20,12 +30,38 @@ import TogglerIcon from './TogglerIcon.vue'
 export default {
   components: { TogglerIcon },
   name: 'Sidebar',
-  
+   
 }
 </script>
 
 <style scoped>
   .nav-button {
     background-color: white
+  }
+
+  .menu-item {
+    font-weight: bold;
+    font-size: 14.5px;
+    color: #777;
+    border: 1px solid #f1f1f1;
+  }
+  .menu-item:hover {
+    cursor: pointer;
+    color: #333;
+  }
+
+  .menu-item-small{
+    font-weight: 200;
+    font-size: 13px;
+    color: #666;
+
+  }
+
+  .angle-down {
+    color: rgb(189, 189, 189);
+    transform: scale(1.4);
+  }
+  .truck {
+    transform: scale(.8);
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,18 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "../node_modules/bootstrap-vue/dist/bootstrap-vue.css";
 import "../src/css/app.css";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faUserSecret,
+  faAngleDown,
+  faTruck,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faUserSecret, faAngleDown, faTruck);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+Vue.config.productionTip = false;
+
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 
