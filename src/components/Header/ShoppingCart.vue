857a9bc91847d859af50468db801ca76e4b0d487
markobezic <marko.bezic@gmail.com>
2021-02-12 13:19:50 -0500

    removed bootstrap-vue sidebar compnonent and added my own

--- src/components/Header/ShoppingCart.vue
+++ src/components/Header/ShoppingCart.vue
@@ -4,11 +4,7 @@
     <button class="cart mt-2 mr-1" v-b-toggle.sidebar-right><img class="m-2" src="https://d24zxkcptsuti0.cloudfront.net/assets/nav/cartGray_thin-8fe991da39ceeb0ad62fb61399527f5414bb7b46f6b544b57536e9c934e6cce8.png" alt="shopping cart icon"/></button>
     <b-sidebar id="sidebar-right" title="Shopping Cart" right shadow>
       <div class="px-3 py-2">
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-          in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-        </p>
-        <b-img src="/src/images/Baggu-maskbagMadras18x13.jpg" fluid thumbnail></b-img>
+        SHOPPING CART GOES HERE
       </div>
     </b-sidebar>
   </div>
--- src/components/Header/Sidebar.vue
+++ src/components/Header/Sidebar.vue
@@ -1,49 +1,79 @@
   <template>
   <div>
-    <button class="nav-button border-0 mt-1" v-b-toggle.sidebar-1><toggler-icon/></button>
-    <b-sidebar id="sidebar-1" shadow>
-      <div class="menu-item px-3 py-2">ALREADY A MEMBER? LOG-IN </div>
-      <div class="menu-item px-3 py-2">SUBCRIBE<font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></div>
-      <div class="menu-item px-3 py-2">ESSENTIALS</div>
-      <div class="menu-item px-3 py-2">NEW </div>
-      <div class="menu-item px-3 py-2">GIFTS <font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></div>
-      <div class="menu-item px-3 py-2">KITCHEN<font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></div>
-      <div class="menu-item px-3 py-2">ON THE GO <font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></div>
-      <div class="menu-item px-3 py-2">CLEANING <font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></div>
-      <div class="menu-item px-3 py-2">BODY <font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></div>
-      <div class="menu-item px-3 py-2">WELLNESS <font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></div>
-      <div class="menu-item px-3 py-2">KIDS <font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></div>
-      <div class="menu-item px-3 py-2">PETS <font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></div>
-      <div class="menu-item px-3 py-2">SALE <font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></div>
-      <div class="menu-item-small px-3 pt-4"><font-awesome-icon class="truck mr-1 mt-1" icon="truck"></font-awesome-icon>Free Shipping over $50</div>
-      <div class="menu-item px-3 py-2">MIGHTLIFE BLOG</div>
-      <div class="menu-item px-3 py-2">LOG-IN</div>
-      
-    </b-sidebar>
+    <button class="nav-button border-0 mt-1"  @click="toggleSlide"><toggler-icon /></button>
+      <div class="sidebar-left" :class="[!isOpen ? 'closedSidebar' : 'openSidebar']">
+        <a class="menu-item d-block px-3 py-2">ALREADY A MEMBER? LOG-IN </a>
+        <a class="menu-item d-block px-3 py-2">SUBCRIBE<font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></a>
+        <a class="menu-item d-block px-3 py-2">ESSENTIALS</a>
+        <a class="menu-item d-block px-3 py-2">NEW </a>
+        <a class="menu-item d-block px-3 py-2">GIFTS <font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></a>
+        <a class="menu-item d-block px-3 py-2">KITCHEN<font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></a>
+        <a class="menu-item d-block px-3 py-2">ON THE GO <font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></a>
+        <a class="menu-item d-block px-3 py-2">CLEANING <font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></a>
+        <a class="menu-item d-block px-3 py-2">BODY <font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></a>
+        <a class="menu-item d-block px-3 py-2">WELLNESS <font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></a>
+        <a class="menu-item d-block px-3 py-2">KIDS <font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></a>
+        <a class="menu-item d-block px-3 py-2">PETS <font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></a>
+        <a class="menu-item d-block px-3 py-2">SALE <font-awesome-icon class="angle-down float-right mt-1" icon="angle-down"></font-awesome-icon></a>
+        <a class="menu-item-small d-block px-3 pt-4"><font-awesome-icon class="truck mr-1 mt-1" icon="truck"></font-awesome-icon>Free Shipping over $50</a>
+        <a class="menu-item d-block px-3 py-2">MIGHTLIFE BLOG</a>
+        <a class="menu-item d-block px-3 py-2">LOG-IN</a>
+      </div>
   </div>
 </template>
 
 <script>
 import TogglerIcon from './TogglerIcon.vue'
+// import TogglerIcon from './TogglerIcon.vue'
 
 
 export default {
   components: { TogglerIcon },
   name: 'Sidebar',
+  data() {
+      return {
+        isOpen: false,
+      }
+  },
+  methods: {
+    toggleSlide(e){
+      e.preventDefault()
+      this.isOpen = !this.isOpen;
+      console.log(this.isOpen)
+    }
+  }
    
 }
 </script>
 
 <style scoped>
+  
+  .closedSidebar{
+    transform: translateX(-102%);
+    transition: .7s;
+  }
+  .openSidebar {
+    transform: translateX(-3%);
+    transition: .7s;
+  }
+  
+  .sidebar-left {
+    z-index: 1;
+    position: absolute;
+    margin-top: 10px;
+    height: 100%;
+  }
   .nav-button {
     background-color: white
   }
 
   .menu-item {
+    text-decoration: none;
     font-weight: bold;
     font-size: 14.5px;
     color: #777;
     border: 1px solid #f1f1f1;
+    background: white;
   }
   .menu-item:hover {
     cursor: pointer;
@@ -51,6 +81,7 @@ export default {
   }
 
   .menu-item-small{
+    text-decoration: none;
     font-weight: 200;
     font-size: 13px;
     color: #666;
